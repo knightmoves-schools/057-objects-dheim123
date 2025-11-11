09e6a9439184c527ea6f454d182564a0c183524e
dheim123 <dheim@midland.k12.ia.us>
2025-11-11 12:09:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-let school = {
-  'name': 'Jim',
-  'average class size': 20,
-  'mascot': 'dog',
+const school = {
+    name: "Lincoln High School",
+    "average class size": 25,
+    mascot: "lion"
 };
 
-document.getElementById('result').innerHTML = `${name} has an average class size of ${average class size} and their mascot is a ${mascot}`;
+const name = school.name;
+const averageClassSize = school["average class size"];
+const mascot = school.mascot;
+
+document.getElementById('result').innerHTML = `${name} has an average class size of ${averageClassSize} and their mascot is a ${mascot}`;
+
 
 
