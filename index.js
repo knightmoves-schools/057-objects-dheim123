1f7eda6b3f558c7cc475c7628b4b6553b07ee2a2
dheim123 <dheim@midland.k12.ia.us>
2025-11-11 12:01:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+let school = {
+  'name': 'Jim',
+  'average class size': 20,
+  'mascot': 'dog',
+};
+
+document.getElementById('result').innerHTML = '${name} has an average class size of ${average class size} and their mascot is a ${mascot}';
 
